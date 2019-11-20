ba522d488f8e00d2b75a62abb4758269563694ce
user01 <js2067@york.ac.uk>
2019-11-20 10:28:19 +0000

    updated to include exercise 4

--- x01.py
+++ x01.py
@@ -3,56 +3,80 @@
 #count = 0
 #while count < 4:
 #    for i in gases:
-#        print (i)
+#        print(i)
 #    count += 1
 
 # Exercise 2.3
 
 #if name == 'Lisa':
-#    print ('plays saxophone')
+#    print('plays saxophone')
 #elif name == 'Bart':
-#    print ('rides skateboard')
+#    print('rides skateboard')
 #else:
-#    print (name) + ('lives in Springfield')
+#    print(name) + ('lives in Springfield')
 
 #Exercise 3.1
-x = [1, 2, 3, 4, 5]
+#x = [1, 2, 3, 4, 5]
 
-second = x[1:2]
-second_to_last = x[3:4]
-complete_list = x[0:5]
-second_to_fourth = x[1:4]
+#second = x[1]
+#second_to_last = x[3]
+#complete_list = x[0:5]
+#second_to_fourth = x[1:4]
 
-print (str(second) + " " + str(second_to_last) + " " + str(complete_list) + " " + str(second_to_fourth))
+#print(str(second) + " " + str(second_to_last) + " " + str(complete_list) + " " + str(second_to_fourth))
 
 #Exercise 3.2
 
-y = list(range(11))
-print (y)
-y[0] = 10
-print (y)
-y.append(11)
-print (y)
-y.extend([12, 13, 14])
-print (y)
+#y = list(range(11))
+#print(y)
+#y[0] = 10
+#print(y)
+#y.append(11)
+#print(y)
+#y.extend([12, 13, 14])
+#print(y)
 
 # Exercise 3.3
 
-forward = []
-backward = []
-values = ['a', 'b', 'c']
-for i in values:
-    forward.append(i)
-    backward.insert(0, i)
-print (forward)
-print (backward)
-forward_reversed = forward.reverse()
-if forward_reversed == backward:
-    print ("Lists are the same!")
-else:
-    print ("Lists are not the same!")
+#forward = []
+#backward = []
+#values = ['a', 'b', 'c']
+#for i in values:
+#    forward.append(i)
+#    backward.insert(0, i)
+#print(forward)
+#print(backward)
+#forward.reverse()
+
+#if forward == backward:
+#    print("Lists are the same!")
+#else:
+#    print("Lists are not the same!")
 
 # Exercise 3.4
 
+#countries = ['uk', 'usa', 'uk', 'uae']
+#print(countries.count('uk'))
 
+#-------------------------------
 
+#Exercise 4.1
+
+t = (1,)
+print(t[0])
+y = list(range(100, 200))
+tuple_list = tuple(y)
+print (str(tuple_list[0]) + " " + str(tuple_list[99]))
+       
+#Exercise 4.2
+
+mylist = [23, 'hi', 2.4e-10]
+for (count, item) in enumerate(mylist):
+    print(count, item)
+    
+#Exercise 4.3
+
+first, middle, last = mylist
+print(str(first) + ' ' + str(middle) + ' ' + str(last))
+first, middle, last = middle, last, first
+print(str(first) + ' ' + str(middle) + ' ' + str(last))
