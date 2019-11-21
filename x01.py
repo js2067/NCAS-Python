19c9162267b75b6668aa290f32bac43eddd79e02
user01 <js2067@york.ac.uk>
2019-11-21 17:01:46 +0000

    Updated to include exercises from Thursday session

--- x01.py
+++ x01.py
@@ -273,7 +273,204 @@
 #print(d.values())
 #print(d.get('maggie'
 
+#--------------------------------- NumPY Arrays
+
+#import numpy as np
+
+#Exercise 1.1
+
+#x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+#a1 = np.array([x], dtype = np.int32)
+#a2 = np.array([x], dtype = np.float64)
+
+#print(f'{a1} {a1.dtype}')
+#print(f'{a2} {a2.dtype}')
+
+#print(' ')
+
+#Exercise 1.2
+#zeros = np.zeros((2,3,4))
+#print(zeros)
+#ones = np.ones((2,3,4))
+#print(ones)
+#one_to_alot = np.arange((1000))
+#print(one_to_alot)
+
+#print(' ')
+
+#Exercise 1.3
+#a = np.array([2, 3.2, 5.5, -6.4, -2.2, 2.4])
+#print(a[1])
+#print(a[1:4])
+
+#print(' ')
+
+#a = np.array([[2, 3.2, 5.5, -6.4, -2.2, 2.4], 
+#	      [1, 22, 4, 0.1, 5.3, -9],
+#	      [3, 1, 2.1, 21, 1.1, -2]])
+
+#print(a[:, 3])
+#print(a[1:4, 0:4])
+#print(a[1:, 2])
+
+#--------------------------
+      
+#Exercise 2.1
+
+#import numpy as np
+
+#arr = np.array([range(4), range(10, 14)])
+#print(np.shape(arr))
+#print(np.size(arr))
+#print(np.max(arr))
+#print(np.min(arr))
+
+#print (' ')
+
+#Exercise 2.2
+
+#print(np.reshape(arr, (2,2,2)))
+#print(np.transpose(arr))
+#print(np.ravel(arr))
+#b = arr.astype(np.float64)
+#print(b)
+
+
+#---------------------
+
+#Exercise 3.1
+
+#import numpy as np
+
+#print(' ')
+
+#a = np.array([range(4), range(10, 14)])
+#b = np.array([2, -1, 1, 0])
+#print(a * b)
+#b1 = b * 100
+#b2 = b * 100.0
+#print(b1)
+#print(b2)
+#print(b1 == b2)
+
+#print(' ')
+
+#Exercise 3.2
+
+#arr = np.array([range(10)])
+
+#print(arr < 3)
+
+#condition = np.logical_or(arr < 3, arr > 8)
+#logic = np.where(condition, arr * 5, arr - 5)
+#print(logic)
+
+#print(' ')
+
+#Exercise 3.3
+
+#def Total(u, v):
+#    TotalMagnitude = ((u**2) + (v**2)) ** 0.5
+#    condition = np.logical(TotalMagnitude > 0.1)
+#    result =  np.where(condition, TotalMagnitude, 0.1)
+#    print(result)
+    
+#u = np.array([[4, 5, 6], [2, 3, 4]])
+#v = np.array([[2, 2, 2], [1, 1, 1]])
+#print(Total(u, v)
+
+#u = np.array([[4, 5, 0.01], [2, 3, 4]])
+#v = np.array([[2, 2, 0.03], [1, 1, 1]])
+#print(Total(u, v))
+
 #---------------------------------
+
+#import numpy as np
+#import numpy.ma as MA
+
+#Exercise 4.1
+
+#print(' ')
+
+#marr = MA.masked_array(range(10),fill_value = -999)
+#print(marr)
+#marr[2] = MA.masked
+#print(marr)
+#print(marr.mask)
+
+#narr = MA.masked_where(marr > 6, marr)
+#print(narr)
+#print(narr.fill_value)
+
+#print (' ')
+
+
+#Exercise 4.2
+
+#m1 = MA.masked_array(range(1, 9))
+#print(m1)
+#m2 = np.reshape(m1, (2,4))
+#print(m2)
+#m3 = MA.masked_where(marr < 7, marr)
+#print(m3 * 100)
+		     
+#---------------------------- Matplotlib
+
+import matplotlib.pyplot as plt
+
+#Exercise 1.1
+
+#plt.plot(range(10))
+#plt.pause(1)
+      
+#Exercise 1.2 / 1.3
+
+#time = [0, 1, 2, 3, 4, 5, 6]
+#CO2_conc = [250, 265, 272, 260, 300, 320, 389]
+#temp = [14.1, 15.5, 16.3, 18.1, 17.3, 19.1, 20.2]
+
+#plt.plot(time, CO2_conc, color = 'blue', ls = '--')
+#plt.plot(time, temp, color = 'red', ls = '--')
+#plt.title('Variation in CO2 levels over time')
+#plt.ylabel('CO2 concentration / ppm')
+#plt.xlabel('Time / decade')
+#plt.savefig('Exercise1.png')
+#plt.show()
+
+#Exercise 2.1
+
+#fig, ax1 = plt.subplots()
+#time = range(7)
+#CO2_conc = [250, 265, 272, 260, 300, 320, 389]
+#temp = [14.1, 15.5, 16.3, 18.1, 17.3, 19.1, 20.2]
+
+#ax1.plot(time, CO2_conc, color = 'red')
+#ax1.set_ylabel('[CO2]', color = 'red')
+#ax2 = ax1.twinx()
+#ax2.plot(time, temp)
+#ax2.set_ylabel('Temp / degC')
+#ax1.set_xlabel('Time / decade')
+#plt.show()
+
+#Exercise 2.2
+
+plt.subplot(1, 3, 1)
+x = (range(0, 10, 1))
+plt.plot(x)
+plt.subplot(1, 3, 2)
+y = (range(10, 0, -1))
+plt.plot(y)
+plt.subplot(1, 3, 3)
+z = ([4] * 10)
+plt.plot(z)
+plt.show()
+
+
+
+
+
+
+
 
 
 
